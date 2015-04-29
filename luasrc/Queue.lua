5ccb069b992cc94602feef01c2f2d41ddd9f57c1
Dustin Shaffer <dustin.n.shaffer@gmail.com>
2015-04-28 22:21:13 -0700

    - Refactoring of Queue

--- luasrc/Queue.lua
+++ luasrc/Queue.lua
@@ -1,33 +1,49 @@
 local public = {}
 
+local size = function(thisQueue)
+  return #(thisQueue.elements)
+end
+
+local enqueue = function(thisQueue, element)
+  table.insert(thisQueue.elements, element)
+end
+
+local dequeue = function(thisQueue)
+  return table.remove(thisQueue.elements, 1)
+end
+
+local peek = function(thisQueue)
+  return thisQueue.elements[1]
+end
+
+local isEmpty = function(thisQueue)
+  return thisQueue:size() == 0
+end
+
+local toString = function(thisQueue)
+  return table.concat(thisQueue.elements, "\n")
+end
+
+local get = function(thisQueue, index)
+  return thisQueue.elements[index]
+end
+
+local clear = function(thisQueue)
+  thisQueue.elements = {}
+end
+
 local function new(this) 
 
     local newQueue = {
       elements = {},
-      size = function(thisQueue)
-        return #(thisQueue.elements)
-      end,
-      enqueue = function(thisQueue, element)
-        table.insert(thisQueue.elements, element)
-      end,
-      dequeue = function(thisQueue)
-        return table.remove(thisQueue.elements, 1)
-      end, 
-      peek = function(thisQueue)
-        return thisQueue.elements[1]
-      end,
-      isEmpty = function(thisQueue)
-        return thisQueue:size() == 0
-      end,
-      toString = function(thisQueue)
-        return table.concat(thisQueue.elements, "\n")
-      end,
-      get = function(thisQueue, index)
-        return thisQueue.elements[index]
-      end,
-      clear = function(thisQueue)
-        thisQueue.elements = {}
-      end
+      size = size,
+      enqueue = enqueue,
+      dequeue = dequeue, 
+      peek = peek,
+      isEmpty = isEmpty,
+      toString = toString,
+      get = get,
+      clear = clear
     }
 
     return newQueue 
