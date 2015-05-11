14c0344bbcca4dbf6877f232a2b9af7737b11c98
dustin <misterdustinface@gmail.com>
2015-05-11 14:58:53 -0700

    - Added more pickups

--- levels/baselevelPopulate.lua
+++ levels/baselevelPopulate.lua
@@ -2,10 +2,17 @@ local populationTable = {
     { class = "Player", row = 24, col = 15, name = "PLAYER1",   traversable = { "FLOOR" } },
     { class = "Enemy",  row = 15, col = 14, name = "FRIENEMY",  traversable = { "FLOOR", "ENEMY_SPAWN" }, speed = 0.5 },
     { class = "Enemy",  row = 15, col = 15, name = "FRIENEMY2", traversable = { "FLOOR", "ENEMY_SPAWN" }, speed = 0.3 },
-    { class = "Pickup", row = 2, col = 2, name = "GOAL", traversable = { "FLOOR" }},
-    { class = "Pickup", row = 21, col = 8, name = "GOAL2", traversable = { "FLOOR" }},
-    { class = "Pickup", row = 2, col = 20, name = "GOAL3", traversable = { "FLOOR" }},
-    { class = "Pickup", row = 21, col = 27, name = "GOAL4", traversable = { "FLOOR" }},
+    { class = "Pickup", row = 2,  col = 2,  name = "GOAL",      traversable = { "FLOOR" }},
+    { class = "Pickup", row = 21, col = 8,  name = "GOAL2",     traversable = { "FLOOR" }},
+    { class = "Pickup", row = 2,  col = 20, name = "GOAL3",     traversable = { "FLOOR" }},
+    { class = "Pickup", row = 21, col = 27, name = "GOAL4",     traversable = { "FLOOR" }},
+    { class = "Pickup", row = 15, col = 7,  name = "GOAL5",     traversable = { "FLOOR" }},
+    { class = "Pickup", row = 15, col = 1,  name = "GOAL6",     traversable = { "FLOOR" }},
+    { class = "Pickup", row = 15, col = 28, name = "GOAL7",     traversable = { "FLOOR" }},
+    { class = "Pickup", row = 15, col = 21, name = "GOAL8",     traversable = { "FLOOR" }},
+    { class = "Pickup", row = 30, col = 15, name = "GOAL9",     traversable = { "FLOOR" }},
+    { class = "Pickup", row = 12, col = 15, name = "GOAL10",    traversable = { "FLOOR" }},
+    { class = "Pickup", row = 6,  col = 15, name = "GOAL11",    traversable = { "FLOOR" }},
 }
 
 local board = GAME:getTiledBoard()
