876be0b576dc0405d1e2a4304e645419a2a11353
Dustin Shaffer <dustin.n.shaffer@gmail.com>
2015-06-06 15:52:27 -0700

    - more modifications to demo

--- levels/largeroomPopulate.lua
+++ levels/largeroomPopulate.lua
@@ -1,6 +1,7 @@
 local populationTable = {
     { class = "Player",    row = 8,  col = 13, name = "PLAYER1",   traversable = { "FLOOR" } },
-    { class = "Enemy",     row = 8,  col = 2,  name = "BLINKY",    traversable = { "FLOOR", "ENEMY_SPAWN" }, speed = 0.75 },
+    { class = "Enemy",     row = 8,  col = 2,  name = "GUNKY",     traversable = { "FLOOR", "ENEMY_SPAWN" }, speed = 0.5 },
+    { class = "Enemy",     row = 10, col = 30, name = "INKY",      traversable = { "FLOOR", "ENEMY_SPAWN" }, speed = 0.75 },
     { class = "Energizer", row = 2,  col = 2,  name = "ENERGIZER", traversable = { "FLOOR" } },
     { class = "Pellet",    row = 2,  col = 40, name = "PELLET",    traversable = { "FLOOR" } },
     { class = "Pellet",    row = 20, col = 40, name = "PELLET2",   traversable = { "FLOOR" } },
